1d20d17924e90e81af80696b9ea1a84949b7a2ed
Dutoit Cédric <dutoitc@shimbawa.ch>
2016-01-31 23:01:26 +0100

    New project

---
+++ src/main/java/ch/mno/copper/process/GroovyProcessor.java
@@ -0,0 +1,7 @@
+package ch.mno.copper.process;
+
+/**
+ * Created by dutoitc on 31.01.2016.
+ */
+public class GroovyProcessor {
+}
---
+++ src/main/java/ch/mno/copper/report/PushoverReporter.java
@@ -0,0 +1,7 @@
+package ch.mno.copper.report;
+
+/**
+ * Created by dutoitc on 31.01.2016.
+ */
+public class PushoverReporter {
+}
