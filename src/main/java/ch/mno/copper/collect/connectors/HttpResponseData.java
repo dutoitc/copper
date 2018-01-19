bdbf3e3f289f36d8cda517a638af28dba810b9b0
Cédric Dutoit <cedric.dutoit@vd.ch>
2018-01-19 13:45:06 +0100

    Http: added user/pass Mail: added values substitution Fixed some NPE Fixed 'WHEN' in story Added some log Added some tests

---
+++ src/main/java/ch/mno/copper/collect/connectors/HttpResponseData.java
@@ -0,0 +1,46 @@
+package ch.mno.copper.collect.connectors;
+
+import org.apache.http.Header;
+
+/**
+ * Created by xsicdt on 12/10/17.
+ */
+public class HttpResponseData<O> {
+
+    private O o;
+    private int responseCode;
+    private String contentLength;
+    private String contentType;
+
+    public void setData(O o) {
+        this.o = o;
+    }
+
+    public O getData() {
+        return o;
+    }
+
+    public void setResponseCode(int responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public void setContentLength(String contentLength) {
+        this.contentLength = contentLength;
+    }
+
+    public String getContentLength() {
+        return contentLength;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+}
