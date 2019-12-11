2132b5f88aab2fdac7bd11a9a82b74f275d51287
Jean-Eric Cuendet <jean-eric.cuendet@vd.ch>
2019-12-11 07:19:16 +0100

    Fix javadoc

--- src/main/java/ch/mno/copper/web/filters/CORSFilter.java
+++ src/main/java/ch/mno/copper/web/filters/CORSFilter.java
@@ -9,16 +9,6 @@ import java.io.IOException;
 @Service
 public class CORSFilter implements Filter {
 
-    /**
-     * Constructor accepting a {@link CorsConfigurationSource} used by the filter
-     * to find the {@link CorsConfiguration} to use for each incoming request.
-     *
-     * @param configSource
-     * @see UrlBasedCorsConfigurationSource
-     */
-//    public CORSFilter(CorsConfigurationSource configSource) {
-//        super(configSource);
-//    }
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         filterChain.doFilter(servletRequest, servletResponse);
