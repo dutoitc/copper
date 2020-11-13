9605e987af21038c13185861299f4d9c1a93a1db
xsicdt <cedric.dutoit@vd.ch>
2020-11-13 09:45:46 +0100

    devex

--- refmon/devex.sh
+++ refmon/devex.sh
@@ -4,4 +4,6 @@ export DEVEX_URL=http://outils-devex-ws.etat-de-vaud.ch/outils/devex
 export DEVEX_USERNAME=gvd0jenkinsrefent
 export DEVEX_PASSWORD=9r8fc4k9m6b239tcbZ38
 
-mvn -U -Pxfile,devex.release,devex.in clean install
+mvn -U clean install
+cp ../target/copper-*.jar ../target/copper.jar
+mvn -U -DskipTests -Pxfile,devex.release,devex.in package
