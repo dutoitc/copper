b5013446c59f2f245dc325901e6572b60e89a708
xsicdt <cedric.dutoit@vd.ch>
2021-10-05 13:56:11 +0200

    Cleanup

--- refmon/devex.sh
+++
@@ -1,9 +0,0 @@
-# c.f. https://portail.etat-de-vaud.ch/outils/dsiwiki/display/CEIAEP/Application+Embedded
-
-export DEVEX_URL=http://outils-devex-ws.etat-de-vaud.ch/outils/devex
-export DEVEX_USERNAME=gvd0jenkinsrefent
-export DEVEX_PASSWORD=9r8fc4k9m6b239tcbZ38
-
-mvn -U clean install
-cp ../target/copper-*.jar ../target/copper.jar
-mvn -U -DskipTests -Pxfile,devex.release,devex.in package
